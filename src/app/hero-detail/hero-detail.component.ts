aaaa9c068a36a533fcafb8a88f36f5ca9d22ddaf
celso <celsofranca@dcc.ufmg.br>
2020-03-14 19:14:12 -0300

    the journey has begun.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {ActivatedRoute} from '@angular/router';
+import {HeroService} from '../hero.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() hero: Hero;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
